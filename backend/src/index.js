bbd18d9b1cf9a381aaa13fb9f183fb5b5c6eccf4
Himanshu <himanshugupta8582@gmail.com>
2026-07-17 23:53:04 +0530

    resolve cors error for development and deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ import { app, server } from "./lib/socket.js";
 
 
 const PORT = process.env.PORT;
-const FRONTEND_URL = process.env.FRONTEND_URL;
+
 const publicDir = path.join(process.cwd(), 'public');
 
 // const app = express();
@@ -33,10 +33,13 @@ const publicDir = path.join(process.cwd(), 'public');
 app.use('/api/webhooks/clerk', express.raw({ type: "application/json" }), clerkWebhook);
 
 app.use(express.json());
-app.use(cors({
-    origin: FRONTEND_URL,
-    credentials: true,
-}));
+
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+  credentials: true,
+};
+
+app.use(cors({corsOptions}));
 app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
